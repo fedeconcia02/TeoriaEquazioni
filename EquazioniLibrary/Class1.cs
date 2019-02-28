4f83e99b430b59edc6af954ebba89f4f8558b0b0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-02-28 09:04:04 +0100

    Ho sviluppato il primo metodo per verificare se l'equazione di 1° grado è determinata.

--- EquazioniLibrary/Class1.cs
+++ EquazioniLibrary/Class1.cs
@@ -8,5 +8,21 @@ namespace EquazioniLibrary
 {
     public class Class1
     {
+        //Metodo per verificare se l'equazione di primo grado è DETERMINATA
+        static bool IsDetermined(double a, double b)
+        {
+            bool analisi = true; //Variabile di tipo 'bool' che stabilisce se l'equazione analizzata è DETERMINATA
+
+            if (a!=0 && b!=0) //Un'equazione di primo grade è DETERMINATA se i coefficienti 'a' e 'b' sono diversi da 0
+            {
+                analisi = true;
+            }
+            else
+            {
+                analisi = false;
+            }
+
+            return analisi;
+        }
     }
 }
