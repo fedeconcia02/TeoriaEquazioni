6beebab117b9a7fe5701d8cdc55e98901d0fa19b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-14 09:02:12 +0100

    Programma ultimato con ogni test per ogni metodo.

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -1,14 +1,592 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
 
 namespace EquazioniLibrary.Test
 {
     [TestClass]
     public class UnitTest1
     {
+        //[TestMethod] per il primo metodo
+        //Test con coefficente "a" positivo
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_IsDetermined1()
         {
+            double a = 1;
+
+            bool risultato = true;
+            bool determinata = EquazioniLibrary.Metodi.IsDetermined(a);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con coefficente "a" uguale a 0
+        [TestMethod]
+        public void TestMethod_IsDetermined2()
+        {
+            double a = 0;
+
+            bool risultato = false;
+            bool determinata = EquazioniLibrary.Metodi.IsDetermined(a);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con coefficente "a" negativo
+        [TestMethod]
+        public void TestMethod_IsDetermined3()
+        {
+            double a = -1;
+
+            bool risultato = true;
+            bool determinata = EquazioniLibrary.Metodi.IsDetermined(a);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+
+        //------------------------------------------------------------------------------------
+
+        //[TestMethod] per il secondo metodo
+        //Test con tutti i coefficenti uguali a 0
+        [TestMethod]
+        public void TestMethod_IsInconsisted1()
+        {
+            double a = 0, b = 0;
+
+            bool equazione = false;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+        //Test con il coefficente "a" positivo e "b" uguale a 0
+        [TestMethod]
+        public void TestMethod_IsInconsisted2()
+        {
+            double a = 4, b = 0;
+
+            bool equazione = false;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+        //Test con il coefficente "a" uguale a 0 e "b" positivo
+        [TestMethod]
+        public void TestMethod_IsInconsisted3()
+        {
+            double a = 0, b = 5;
+
+            bool equazione = true;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+        //Test con il coefficente "a" negativo e "b" uguale a 0
+        [TestMethod]
+        public void TestMethod_IsInconsisted4()
+        {
+            double a = -4, b = 0;
+
+            bool equazione = false;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+        //Test con il coefficente "a" uguale a 0 e "b" negativo
+        [TestMethod]
+        public void TestMethod_IsInconsisted5()
+        {
+            double a = 0, b = -5;
+
+            bool equazione = true;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+        //Test con il coefficente "a" positivo e "b" negativo
+        [TestMethod]
+        public void TestMethod_IsInconsisted6()
+        {
+            double a = 4, b = -5;
+
+            bool equazione = false;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+        //Test con il coefficente "a" negativo e "b" positivo
+        [TestMethod]
+        public void TestMethod_IsInconsisted7()
+        {
+            double a = -4, b = 5;
+
+            bool equazione = false;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+        //Test con tutti i coefficenti positivi
+        [TestMethod]
+        public void TestMethod_IsInconsisted8()
+        {
+            double a = 4, b = 5;
+
+            bool equazione = false;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+        //Test con tutti i coefficenti negativi
+        [TestMethod]
+        public void TestMethod_IsInconsisted9()
+        {
+            double a = -4, b = -5;
+
+            bool equazione = false;
+            bool risultato = EquazioniLibrary.Metodi.IsInconsisted(a, b);
+
+            Assert.AreEqual(equazione, risultato);
+        }
+
+        //------------------------------------------------------------------------------------
+
+        //[TestMethod] per il terzo metodo
+        //Test con coefficente "a" positivo
+        [TestMethod]
+        public void TestMethod_IsDegree1()
+        {
+            double a = 4;
+
+            bool risultato = true;
+            bool determinata = EquazioniLibrary.Metodi.IsDegree2(a);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con coefficente "a" uguale a 0
+        [TestMethod]
+        public void TestMethod_IsDegree2()
+        {
+            double a = 0;
+
+            bool risultato = false;
+            bool determinata = EquazioniLibrary.Metodi.IsDegree2(a);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con coefficente "a" negativo
+        [TestMethod]
+        public void TestMethod_IsDegree3()
+        {
+            double a = -34;
+
+            bool risultato = false;
+            bool determinata = EquazioniLibrary.Metodi.IsDegree2(a);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+
+        //------------------------------------------------------------------------------------
+
+        //[TestMethod] per il quarto metodo
+        //Test con tutti i coefficenti positivi
+        [TestMethod]
+        public void TestMethod_Delta1()
+        {
+            double a = 4, b = 5, c = 6;
+
+            double risultato = -71;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con coefficente "a" uguale a 0 e i coefficenti "b" e "c" positivi
+        [TestMethod]
+        public void TestMethod_Delta2()
+        {
+            double a = 0, b = 5, c = 6;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con coefficente "a" e "c" positivi e il coefficente "b" uguale a 0
+        [TestMethod]
+        public void TestMethod_Delta3()
+        {
+            double a = 4, b = 0, c = 6;
+
+            double risultato = -96;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con coefficenti "a" e "b" positivi e il coefficente "c" uguale a 0
+        [TestMethod]
+        public void TestMethod_Delta4()
+        {
+            double a = 4, b = 5, c = 0;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con coefficente "a" negativo e i coefficenti "b" e "c" positivi
+        [TestMethod]
+        public void TestMethod_Delta5()
+        {
+            double a = -4, b = 5, c = 6;
+
+            double risultato = 121;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con  i coefficenti "a" e "c" positivi e il coefficente "b" negativo
+        [TestMethod]
+        public void TestMethod_Delta6()
+        {
+            double a = 4, b = -5, c = 6;
+
+            double risultato = -71;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "b" positivi e il coefficente "c" negativo
+        [TestMethod]
+        public void TestMethod_Delta7()
+        {
+            double a = 4, b = 5, c = -6;
+
+            double risultato = 121;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+
+        //Test con tutti i coefficenti uguali a 0
+        [TestMethod]
+        public void TestMethod_Delta8()
+        {
+            double a = 0, b = 0, c = 0;
+
+            double risultato = 0;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "b" uguali a 0 e il coefficente "c" positivo
+        [TestMethod]
+        public void TestMethod_Delta9()
+        {
+            double a = 0, b = 0, c = 6;
+
+            double risultato = 0;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "c" uguali a 0 e il coefficente "b" positivo
+        [TestMethod]
+        public void TestMethod_Delta10()
+        {
+            double a = 0, b = 5, c = 0;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "b" e "c" uguali a 0 e il coefficente "a" positivo
+        [TestMethod]
+        public void TestMethod_Delta11()
+        {
+            double a = 4, b = 0, c = 0;
+
+            double risultato = 0;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "b" uguali a 0 e il coefficente "c" negativo
+        [TestMethod]
+        public void TestMethod_Delta12()
+        {
+            double a = 0, b = 0, c = -6;
+
+            double risultato = 0;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "c" uguali a 0 e il coefficente "b" negativo
+        [TestMethod]
+        public void TestMethod_Delta13()
+        {
+            double a = 0, b = -5, c = 0;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "b" e "c" uguali a 0 e il coefficente "a" negativo
+        [TestMethod]
+        public void TestMethod_Delta14()
+        {
+            double a = -4, b = 0, c = 0;
+
+            double risultato = 0;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+
+        //Test con tutti i coefficenti negativi
+        [TestMethod]
+        public void TestMethod_Delta15()
+        {
+            double a = -4, b = -5, c = -6;
+
+            double risultato = -71;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "b" negativi e il coefficente "c" positivo
+        [TestMethod]
+        public void TestMethod_Delta16()
+        {
+            double a = -4, b = -5, c = 6;
+
+            double risultato = 121;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "c" negativi e il coefficente "b" positivo
+        [TestMethod]
+        public void TestMethod_Delta17()
+        {
+            double a = -4, b = 5, c = -6;
+
+            double risultato = -71;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "b" e "c" negativi e il coefficente "a" positivo
+        [TestMethod]
+        public void TestMethod_Delta18()
+        {
+            double a = 4, b = 5, c = -6;
+
+            double risultato = 121;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "b" negativi e il coefficente "c" uguale a 0
+        [TestMethod]
+        public void TestMethod_Delta19()
+        {
+            double a = -4, b = -5, c = 0;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" e "c" negativi e il coefficente "b" uguale a 0
+        [TestMethod]
+        public void TestMethod_Delta20()
+        {
+            double a = -4, b = 0, c = -6;
+
+            double risultato = -96;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "b" e "c" negativi e il coefficente "a" uguale a 0
+        [TestMethod]
+        public void TestMethod_Delta21()
+        {
+            double a = 0, b = -5, c = -6;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+
+        //Test con i coefficenti "a" uguale a 0, "b" negativo e "c" positivo
+        [TestMethod]
+        public void TestMethod_Delta22()
+        {
+            double a = 0, b = -5, c = 6;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" uguale a 0, "b" positivo e "c" negativo
+        [TestMethod]
+        public void TestMethod_Delta23()
+        {
+            double a = 0, b = 5, c = -6;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" uguale a 0, "b" negativo e "c" positivo
+        [TestMethod]
+        public void TestMethod_Delta24()
+        {
+            double a = -4, b = 0, c = 6;
+
+            double risultato = 96;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" uguale a 0, "b" negativo e "c" positivo
+        [TestMethod]
+        public void TestMethod_Delta25()
+        {
+            double a = 4, b = 0, c = -6;
+
+            double risultato = 96;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" negativo, "b" positivo e "c" uguale a 0
+        [TestMethod]
+        public void TestMethod_Delta26()
+        {
+            double a = -4, b = 5, c = 0;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+        //Test con i coefficenti "a" positivo, "b" negativo e "c" uguale a 0
+        [TestMethod]
+        public void TestMethod_Delta27()
+        {
+            double a = 4, b = -5, c = 0;
+
+            double risultato = 25;
+            double determinata = EquazioniLibrary.Metodi.Delta(a, b, c);
+
+            Assert.AreEqual(risultato, determinata);
+        }
+
+        //------------------------------------------------------------------------------------
+
+        //[TestMethod] per il quinto metodo
+        //Test con tutti i coefficenti uguali a 0
+        [TestMethod]
+        public void TestMethod_EquationDegree1()
+        {
+            double a = 0, b = 0;
+
+            string x = "Indeterminata";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
+        }
+        //Test con il coefficente "a" positivo e "b" uguale a 0
+        [TestMethod]
+        public void TestMethod_EquationDegree2()
+        {
+            double a = 4, b = 0;
+
+            string x = "0";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
+        }
+        //Test con il coefficente "a" uguale a 0 e "b" positivo
+        [TestMethod]
+        public void TestMethod_EquationDegree3()
+        {
+            double a = 0, b = 5;
+
+            string x = "Impossibile";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
+        }
+        //Test con il coefficente "a" negativo e "b" uguale a 0
+        [TestMethod]
+        public void TestMethod_EquationDegree4()
+        {
+            double a = -4, b = 0;
+
+            string x = "0";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
+        }
+        //Test con il coefficente "a" uguale a 0 e "b" negativo
+        [TestMethod]
+        public void TestMethod_EquationDegree5()
+        {
+            double a = 0, b = -5;
+
+            string x = "Impossibile";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
+        }
+        //Test con il coefficente "a" positivo e "b" negativo
+        [TestMethod]
+        public void TestMethod_EquationDegree6()
+        {
+            double a = 4, b = -5;
+
+            string x = "-1,25";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
+        }
+        //Test con il coefficente "a" negativo e "b" positivo
+        [TestMethod]
+        public void TestMethod_EquationDegree7()
+        {
+            double a = -4, b = 5;
+
+            string x = "-1,25";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
+        }
+        //Test con tutti i coefficenti positivi
+        [TestMethod]
+        public void TestMethod_EquationDegree8()
+        {
+            double a = 4, b = 5;
+
+            string x = "1,25";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
+        }
+        //Test con tutti i coefficenti negativi
+        [TestMethod]
+        public void TestMethod_EquationDegree9()
+        {
+            double a = -4, b = -5;
+
+            string x = "1,25";
+            string risultato = EquazioniLibrary.Metodi.EquationDegree1(a, b);
+
+            Assert.AreEqual(Convert.ToDouble(x), Convert.ToDouble(risultato));
         }
     }
 }
---
+++ EquazioniLibrary/Metodi.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class Metodi
+    {
+        //Metodo per verificare se l'equazione di primo grado è DETERMINATA
+        public static bool IsDetermined(double a)
+        {
+            bool determinata = false; //Variabile di tipo 'bool' che stabilisce se l'equazione analizzata è DETERMINATA
+
+            if (a!=0) //Un'equazione di primo grade è DETERMINATA se i coefficienti 'a' e 'b' sono diversi da 0
+            {
+                determinata = true;
+            }
+
+            return determinata;
+        }
+
+        //Metodo per verificare se l'equazione di primo grado è IMPOSSIBILE
+        public static bool IsInconsisted(double a, double b)
+        {
+            bool impossibile = false; //Variabile di tipo 'bool' che stabilisce se l'equazione analizzata è IMPOSSIBILE
+
+            if (a == 0 && b != 0) //Un'equazione di primo grade è IMPOSSIBILE se il coefficiente 'a' è uguale a 0 e il coefficiente 'b' è diverso da 0
+            {
+                impossibile = true;
+            }
+
+            return impossibile;
+        }
+
+        //Metodo per verificare se l'equazione è di SECONDO GRADO
+        public static bool IsDegree2(double a)
+        {
+            bool secondogrado = true; //Variabile di tipo 'bool' che stabilisce se l'equazione analizzata è di SECONDO GRADO
+
+            if (a == 0 || a < 0) //Un'equazione è di SECONDO GRADO se 
+            {
+                secondogrado = false;
+            }
+
+            return secondogrado;
+        }
+
+        //Metodo per calcolare il delta
+        public static double Delta(double a, double b, double c)
+        {
+            double delta = 0;
+
+            delta = (b * b) - (4 * a * c);
+
+            return delta;
+        }
+
+        //Metodo Per calcolare un'equazione di primo grado
+        public static string EquationDegree1(double a, double b)
+        {
+            string risulato = "";
+            bool tipo = false;
+
+            tipo = IsDetermined(a);
+            if (tipo==true)
+            {
+                risulato = "Indeterminata ";
+            }
+
+            tipo = IsInconsisted(a, b);
+            if (tipo==true)
+            {
+                if (risulato=="Indeterminata")
+                {
+                    risulato = "Indeterminata e impossibile ";
+                }
+                else
+                {
+                    risulato = "Impossibile ";
+                }
+            }
+
+            if (risulato=="")
+            {
+                risulato = Convert.ToString(a / b);
+            }
+
+            return risulato;
+        }
+    }
+}
